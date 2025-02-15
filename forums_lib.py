b398daf450421bfac575f05a1d44e1ab37a2820e
ericericsw <45174574+ericericsw@users.noreply.github.com>
2025-02-15 19:16:21 +0800

    論壇lib初版(有edit能力)

---
+++ forums_lib.py
@@ -0,0 +1,246 @@
+# import libary
+import requests
+import json
+import time
+import datetime
+import re
+import logging
+import os
+import configparser
+import pytz
+import sys
+from typing import List, Dict, TypedDict
+from bs4 import BeautifulSoup
+# endregion
+
+"""
+better coments 格式
+* 000
+! 123
+? 456
+TODO 789
+@param check_folder_path_exists 1234124
+//415646464886
+
+
+# logging.CRITICAL（50）
+# logging.ERROR（40）
+# logging.WARNING（30）
+# logging.INFO（20）
+# logging.DEBUG（10）
+"""
+
+# 指定時區
+timezone = pytz.timezone('Asia/Taipei')
+
+if getattr(sys, 'frozen', False):
+    # 如果是打包後的可執行文件
+    current_directory = os.path.dirname(sys.executable)
+else:
+    # 如果是未打包的原始腳本
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+# 創建 ConfigParser 對象
+config = configparser.ConfigParser()
+# 讀取配置文件
+config_path = os.path.join(current_directory, 'config.ini')
+config.read(config_path, encoding="utf-8")
+
+csv_folder_path = os.path.join(current_directory, 'csv')
+log_folder_path = os.path.join(current_directory, 'log')
+json_folder_path = os.path.join(current_directory, 'json')
+
+# 檢查工作目錄是否有資料夾，沒有的話建立 log 資料夾
+log_dir = os.path.join(current_directory, 'log')
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+    print(f"因資料夾不存在， 建立'{log_dir}' 資料夾。")
+
+# 設置日誌
+Log_Mode = config.get('Log', 'Log_Mode')
+# 日誌格式
+Log_Format = '%(asctime)s | %(filename)s | %(funcName)s | %(levelname)s:%(message)s'
+log_file_path = os.path.join(
+    current_directory, 'log', 'sample.log')
+logging.basicConfig(level=getattr(logging, Log_Mode.upper()),
+                    format=Log_Format,
+                    handlers=[logging.FileHandler(log_file_path, 'a', 'utf-8'),
+                              logging.StreamHandler()])
+
+
+def check_folder_path_exists(folder_Path: os.path):
+    if not os.path.exists(folder_Path):
+        os.makedirs(folder_Path)
+        logging.warning(f"資料夾 '{folder_Path}' 已建立。")
+
+
+class CookieDict(TypedDict):
+    ipb_member_id: str
+    ipb_pass_hash: str
+    ipb_session_id: str
+
+
+def get_cookie() -> CookieDict:
+
+    cookies = {}
+    ipb_member_uid_value = config.get('Account', 'HV_Free_Shop_UID')
+    ipb_pass_hash_value = config.get('Account', 'ipb_pass_hash')
+    ipb_session_id_value = config.get('Account', 'ipb_session_id')
+
+    cookies = {
+        'ipb_member_id': ipb_member_uid_value,
+        'ipb_pass_hash': ipb_pass_hash_value,
+        'ipb_session_id': ipb_session_id_value
+    }
+
+    return cookies
+
+
+class Forums():
+    def __init__(self, cookies: CookieDict):
+        """
+        初始化
+        """
+        self.forums = 'https://forums.e-hentai.org'
+        self.cookies = cookies
+        self.md5check = get_md5check(self.forums)
+
+    def post_edit(self, thread_id, post_number: int, post_text) -> bool:
+        url = self.forums + '/index.php?showtopic=' + str(thread_id)
+        post_id = get_post_id(url, post_number)
+        url = "https://forums.e-hentai.org/index.php?s=&act=xmlout&do=post-edit-save&p={}&t={}&f=4".format(
+            post_id, thread_id)
+
+        headers = {
+            "accept": "*/*",
+            "accept-language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6,zh-CN;q=0.5",
+            "cache-control": "no-cache",
+            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "pragma": "no-cache",
+            "priority": "u=1, i",
+            "sec-ch-ua": "\"Not(A:Brand\";v=\"99\", \"Google Chrome\";v=\"133\", \"Chromium\";v=\"133\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"Windows\"",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin"
+        }
+
+        data = {
+            "md5check": self.md5check,
+            "Post": post_text
+        }
+
+        response = requests.post(url, headers=headers,
+                                 data=data, cookies=get_cookie())
+
+        if response.status_code == 200:
+            return True
+        else:
+            logging.warning('error code:{},error message:{}'.format(
+                response.status_code, response.text))
+            return False
+
+
+def get_post_id(url: str, post_number: int) -> str:
+    """
+    輸入帶thread_id的URL與Post Number，回傳Post ID
+
+    input:
+        url範例:https://forums.e-hentai.org/index.php?showtopic=257252
+        post_number: #2 則輸入 2
+
+    """
+
+    if isinstance(post_number, int) and post_number > 0:
+
+        url = url + '&st=' + str(post_number-1)
+
+        # Send a GET request to the URL
+        response = requests.get(url, cookies=get_cookie())
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Parse the HTML content using BeautifulSoup
+            soup = BeautifulSoup(response.content, 'html.parser')
+
+            # Find all <a> tags with the specified title and href attributes
+            post_links = soup.find_all(
+                'a', title="Show the link to this post", href="#")
+
+            # Dictionary to store the post numbers and their corresponding content
+            posts = {}
+
+            for link in post_links:
+                # Extract the post id from the onclick attribute
+                onclick_attr = link.get('onclick')
+                post_id = onclick_attr.split('(')[1].split(')')[0]
+
+                # Extract the main key (e.g., #1, #2, #3)
+                main_key = link.text.strip().lstrip('#')
+
+                # Store the post id and main key in the dictionary
+                posts[main_key] = post_id
+
+            # Print the results
+            # for key, value in posts.items():
+            #     print(f"{key}: {value}")
+
+            return posts[str(post_number)]
+        else:
+            print(f"Failed to fetch the URL. Status code: {
+                  response.status_code}")
+    else:
+        print(f"{post_number} Not an integer greater than 0")
+
+
+def get_md5check(url) -> str:
+
+    # Send a GET request to the URL
+    response = requests.get(url, cookies=get_cookie())
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Find the script tag containing the ipb_md5_check variable
+        script_tag = soup.find(
+            'script', string=lambda t: t and 'var ipb_md5_check' in t)
+
+        if script_tag:
+            # Extract the JavaScript code
+            script_content = script_tag.string
+
+            # Find the value of ipb_md5_check
+            start_index = script_content.find(
+                'var ipb_md5_check') + len('var ipb_md5_check') + 3
+            end_index = script_content.find(';', start_index) - 1
+            ipb_md5_check = script_content[start_index:end_index].strip().strip(
+                '"').strip('= "')
+
+            # print(ipb_md5_check)
+            return ipb_md5_check
+        else:
+            print("Script tag containing 'var ipb_md5_check' not found.")
+    else:
+        print(f"Failed to fetch the URL. Status code: {response.status_code}")
+
+
+def main():
+    # post_test()
+    # temp = get_md5check(
+    #     "https://forums.e-hentai.org/index.php?showtopic=283538")
+    # print(temp)
+    # post_id = get_post_id(
+    #     'https://forums.e-hentai.org/index.php?showtopic=283538', 2)
+    # print(post_id)
+
+    test = Forums(get_cookie())
+    test.post_edit('283538', 2, 'testvs')
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+# %0A
